2389ee010efd2f2ee2eabe70a50d15527bc0095d
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2021-10-06 17:57:59 +0530

    Students Component added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import 'firebase/auth';
 import 'firebase/firestore';
 //components
 import Navbar from "./components/navbar/Navbar";
+//Pages
+import Students from './pages/Students';
+//Router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Students} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,52 +1,33 @@
-import React from 'react'
+import React from "react";
+// import { Link } from "react-router-dom";
+// import { useFirebase } from "react-redux-firebase";
 import Logo from "../../assets/logo.svg";
 const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-sm navbar-light bg-white">
-  <div className="container">
-    <a className="navbar-brand" href="/">
-    <img
-            src={Logo}
-            height="30px"
-            alt="logo"
-          />
-    </a>
-    <div>
-      <ul className="navbar-nav mr-auto"></ul>
-      <ul className="navbar-nav align-items-center">
-        <li className="nav-item">
-          <a href="#!" className="btn btn-primary mr-3">
-            Add Student
-          </a>
-        </li>
-        <li className="nav-item dropdown">
-          <a
-            className="nav-a dropdown-toggle"
-            href="!#"
-            id="navbarDropdown"
-            data-toggle="dropdown"
-          >
-            admin
-            <span className="ml-2 navbar-text">Debadrita Ghosh</span>
-          </a>
-          <div className="dropdown-menu">
-            <a className="dropdown-item" href="!#">
-              Profile
-            </a>
-            <a className="dropdown-item" href="!#">
-              Logout
-            </a>
-            <div className="dropdown-divider"></div>
-            <a className="dropdown-item" href="!#">
-              Ads
-            </a>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    )
+  return (
+    <nav className="navbar navbar-expand-sm navbar-light bg-white">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <img src={Logo} alt="logo" />
+        </a>
+
+        <div>
+          <ul className="navbar-nav mr-auto"></ul>
+          <ul className="navbar-nav align-items-center">
+            <li className="nav-item">
+              <a href="#!" className="btn btn-primary mr-3">
+                Add Student
+              </a>
+            </li>
+            <li className="nav-item">
+              <span className="ml-2 navbar-text px-3">Debadrita Ghosh</span>
+              <span className="ml-2 navbar-text px-3">Profile</span>
+              <span className="ml-2 navbar-text px-3">Logout</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar;
